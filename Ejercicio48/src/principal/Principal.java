1eaf89f4ab194fb50ac6a4367c24a57d11fdc591
rubencerroo <ruben.cerrodiaz@usp.ceu.es>
2022-11-28 13:28:19 +0100

    innitial commit

---
+++ Ejercicio48/src/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+import java.util.*;
+public class Principal {
+	public static void main (String [] args) {
+		int a =1;
+		String b= "B";
+		double c=0.25;
+		
+		System.out.println(a+"\n"+b+"\n"+c);
+ }
+}	
